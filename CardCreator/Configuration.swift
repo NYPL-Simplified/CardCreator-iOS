775089c3bce5b5a0e86015144913d7c19313d58d
Winnie Quinn <me@winniequinn.com>
2016-09-12 00:18:01 -0400

    Added completionHandler to Configuration

--- CardCreator/Configuration.swift
+++ CardCreator/Configuration.swift
@@ -3,4 +3,5 @@ import Foundation
 @objc public class Configuration: NSObject {
   public var endpointURL: NSURL = NSURL(string: "http://qa.patrons.librarysimplified.org/v1")!
   public var requestTimeoutInterval: NSTimeInterval = 10.0
+  public var completionHandler: (barcode: String, PIN: String) -> Void = {_ in}
 }
