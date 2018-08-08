c27246f3a696b4f4a266315e58aa442abf7ce13f
Greg O'Neill <gioneill@gmail.com>
2018-08-08 17:28:35 -0400

    add auto-fill/auto-suggest feature to form fields
    Co-authored-by: Vui Nguyen <vui_engineer@yahoo.com>
    Co-authored-by: Greg O'Neill <gioneill@gmail.com>

--- NYPLCardCreator/AddressViewController.swift
+++ NYPLCardCreator/AddressViewController.swift
@@ -114,6 +114,14 @@ final class AddressViewController: FormTableViewController {
     self.zipCell.textField.addTarget(self,
                                      action: #selector(zipTextFieldDidChange),
                                      for: .allEditingEvents)
+
+    if #available(iOS 10.0, *) {
+      self.street1Cell.textField.textContentType = .streetAddressLine1
+      self.street2Cell.textField.textContentType = .streetAddressLine2
+      self.cityCell.textField.textContentType    = .addressCity
+      self.regionCell.textField.textContentType  = .addressState
+      self.zipCell.textField.textContentType     = .postalCode
+    }
   }
   
   func checkToPrefillForm() {
